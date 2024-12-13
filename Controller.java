53b1920835f007a76a7a60acc20fe4bd62c35169
hamabu0531 <hamabu0531@icloud.com>
2024-12-13 16:32:05 +0900

    衝突チェックの実装

--- Controller.java
+++ Controller.java
@@ -2,6 +2,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import javax.swing.*;
 
@@ -21,12 +22,13 @@ public class Controller{
         view.addKeyListener(new KeyListener() {
             @Override
             public void keyPressed(KeyEvent e){
-                if(e.getKeyChar()=='a'){
-                    model.moveToLeft();
-                    System.out.println("Hi");
-                }else if(e.getKeyChar()=='d'){
+                if(e.getKeyCode()==KeyEvent.VK_RIGHT){
                     model.moveToRight();
-                    System.out.println("Hello");
+                }else if(e.getKeyCode()==KeyEvent.VK_LEFT){
+                    model.moveToLeft();
+                }
+                else if(e.getKeyChar()=='e'){
+                    //model.stopGame();
                 }
             }
             public void keyReleased(KeyEvent e){
@@ -37,13 +39,35 @@ public class Controller{
             }
         });
 
+        // 岩生成(テスト)
+        model.setRockInfo(0, 0);
+
+        // 一定時間ごとに岩を移動
+        new Timer(10, new ActionListener() {
+           @Override
+           public void actionPerformed(ActionEvent e){
+            // 岩を移動する関数
+            ArrayList<Integer> al = model.getRockPosY();
+            for(int i=0; i<al.size(); i++){
+                model.increaseRockPosY();
+            }
+
+            // 衝突判定関数
+            if(model.checkCollision()){
+                model.stopGame();
+            }
+           }
+        }).start();
+
+
         new Timer(100, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e){
                 if(model.isGameOver()){
                     // ゲームオーバー処理
-                    view.getScoreLabel().setText("Time Over!");
+                    //System.out.println("You Lose...");
                 }
+                System.out.println("PlayerPosX = " + model.getPlayerPosX());
             }
         }).start();
     }
