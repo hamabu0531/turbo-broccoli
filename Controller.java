be391dc519fa27b4c4e54dcd4b7dc122bfc298f8
hamabu0531 <hamabu0531@icloud.com>
2024-12-13 13:53:57 +0900

    左右移動の有力追加

--- Controller.java
+++ Controller.java
@@ -1,12 +1,14 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.*;
 
 // Cはユーザー入力を処理する(Listener関係)
 // Cは、Mの更新をし、Vにイベントを伝える
 
-public class Controller {
+public class Controller{
     private Model model;
     private View view;
 
@@ -15,6 +17,23 @@ public class Controller {
         this.view = view;
 
         model.startGame();
+
+        view.addKeyListener(new KeyListener() {
+            @Override
+            public void keyPressed(KeyEvent e){
+                if(e.getKeyChar()=='a'){
+                    model.moveToleft();
+                }else if(e.getKeyChar()=='d'){
+                    model.moveToright();
+                }
+            }
+            public void keyReleased(KeyEvent e){
+
+            }
+            public void keyTyped(KeyEvent e){
+        
+            }
+        });
 
         // ボタンがクリックされたときのイベントリスナー
         view.getScoreButton().addActionListener(new ActionListener() {
