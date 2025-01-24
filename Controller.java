978a10c13ea03bd010278387059202779bafcf32
hamabu0531 <hamabu0531@icloud.com>
2025-01-24 12:49:29 +0900

    発表用ブランチの作成

--- Controller.java
+++ Controller.java
@@ -37,9 +37,9 @@ public class Controller {
         deletedItem = 0;
         deletedArmor = 0;
         generateCounter = 50;
-        rockSpawnInterval = 30; // ゲームの難易度で数値変更可
-        itemSpawnInterval = 80; // ゲームの難易度で数値変更可
-        armorSpawnInterval = 500; // ゲームの難易度で数値変更可
+        rockSpawnInterval = 35; // ゲームの難易度で数値変更可
+        itemSpawnInterval = 110; // ゲームの難易度で数値変更可
+        armorSpawnInterval = 550; // ゲームの難易度で数値変更可
         offsetY = -100;
 
         try {
